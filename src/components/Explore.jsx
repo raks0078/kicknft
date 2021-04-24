a8ce4969151430e8774732daf5e9710f6978b9b7
Abhishek Kumar <sharmaabhishek098@gmail.com>
2021-04-24 23:37:03 +0530

    updates

---
+++ src/components/Explore.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./Explore.scss"
+class Explore extends Component {
+  render() {
+    return (
+      <div>
+        Explore.....
+      </div>
+    )
+  }
+}
+
+export default Explore
